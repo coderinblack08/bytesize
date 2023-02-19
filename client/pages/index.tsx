b8fc2484ed250ba288b2fe03ef3eb266213874a8
coderinblack08 <kevinlu.email@gmail.com>
2023-02-19 08:25:23 -0500

    move to client folder
    Co-authored-by: Sameer M <SameeraMurthy@users.noreply.github.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Button, Container, Heading } from "@chakra-ui/react";
 import { NextPage } from "next";
+import React from "react";
 
 const Home: NextPage = () => {
   return (
     <Container>
       <Heading fontSize="4xl">Read ByteSize</Heading>
+      <Button></Button>
     </Container>
   );
 };
