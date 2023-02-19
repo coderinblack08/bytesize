8f094d3a502add94f3360f7d9322f52dbc684ed6
coderinblack08 <kevinlu.email@gmail.com>
2023-02-19 08:21:57 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Heading } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Heading fontSize="4xl">Read ByteSize</Heading>
+    </Container>
+  );
+};
+
+export default Home;
